7c09ba5dfa18b1910471d3b9f5d0a058c6f5532c
DarmstadtiumKKK <31994825+DarmstadtiumKKK@users.noreply.github.com>
2018-04-26 19:09:17 +0300

    Add files via upload

--- classes.py
+++ classes.py
@@ -1,4 +1,5 @@
 from abc import ABCMeta, abstractmethod
+from datetime import datetime, date, time
 
 # class StandData(metaclass=ABCMeta):
 
@@ -17,6 +18,7 @@ class StandStruckt(metaclass=ABCMeta):
 
     def get_data(self):
         return self._data
+
 
     '''@abstractmethod
     def _processing(self, result):
@@ -40,11 +42,40 @@ class Struckt_of_Queue(StandStruckt):
     def creating(self, key):
         self._data[key] = []
 
+    def is_consisting(self, ars):
+        data=self.get_data()
+        list_of_task = data[ars[1]]
+        for i in range(len(list_of_task)):
+            if str(list_of_task[i].get_id()) == ars[2]:
+                return 'YES'
+        return 'NO'
+
+    def get_task(self, queue_name):
+        list_of_task = self.get_data()[queue_name]
+        for i in range(len(list_of_task)):
+            if not list_of_task[i].get_status():
+                self._data[queue_name][i]._status = True
+                list_of_task[i]._date_of_take = datetime.strftime(datetime.now(), "%H:%M:%S")
+                return str(list_of_task[i].get_id()) + ' ' + list_of_task[i].get_length() + ' ' + list_of_task[
+                    i].get_data()
+        return 'NONE'
+
     def poisk_num(self, key, id):
         list_of_task=self._data[key]
         for i in range(len(list_of_task)):
             if str(list_of_task[i].get_id()) == id:
                 return i
+
+    def remove(self, queue_name, task_id):
+        data = self.get_data()
+        data[queue_name].pop(self.poisk_num(queue_name, task_id))
+
+    def _check_timeout(self, timeout=0):
+        current_data = self.get_data()
+        data_values = list(current_data.values())
+        for current_list in data_values:
+            for task in current_list:
+                task.check_timeout(timeout)
 
 
 class Task(StandStruckt):
@@ -60,5 +91,15 @@ class Task(StandStruckt):
 
     def get_num(self):
         return self._num_of_queue
+
+    def check_timeout(self, timeout=0):
+        now_time=datetime.now()
+        if self._date_of_take is not None:
+            past_time = datetime.strptime(self._date_of_take, "%H:%M:%S")
+            delta = now_time - past_time
+            if delta.seconds > timeout:
+                self._status = False
+                self._date_of_take = None
+
 
 
