3315acaea1c2edd41cfe54ac0a28de7eb822f1bb
DarmstadtiumKKK <31994825+DarmstadtiumKKK@users.noreply.github.com>
2018-04-26 19:09:04 +0300

    Add files via upload

--- server.py
+++ server.py
@@ -11,6 +11,7 @@ class Server:
         self.sock.listen(1)
         self.data_of_queue = Struckt_of_Queue('1', 0, {})
         self.sock.setblocking(0)
+        self.timeout = 300
 
     def run(self):
         self._restore()
@@ -27,7 +28,7 @@ class Server:
                         conn.send(bytes(result, 'utf-8'))
                         conn.close()
                         self._archive()
-                self._check_timeout()
+                self.data_of_queue._check_timeout(self.timeout)
         except KeyboardInterrupt:
             return None
 
@@ -40,27 +41,15 @@ class Server:
         return str(id)
 
     def _in(self, ars):
-        data=self.data_of_queue.get_data()
-        list_of_task=data[ars[1]]
-        for i in range(len(list_of_task)):
-            if str(list_of_task[i].get_id()) == ars[2]:
-                return 'YES'
-        return  'NO'
+        return self.data_of_queue.is_consisting(ars)
 
     def _get(self, ars):
-        list_of_task = self.data_of_queue.get_data()[ars[1]]
-        for i in range(len(list_of_task)):
-            if not list_of_task[i].get_status():
-                self.data_of_queue._data[ars[1]][i]._status=True
-                self.data_of_queue._data[ars[1]][i]._date_of_take=datetime.now()
-                return str(list_of_task[i].get_id())+' '+list_of_task[i].get_length()+' '+list_of_task[i].get_data()
-        return 'NONE'
+        return self.data_of_queue.get_task(ars[1])
 
     def _ack(self, ars):
         check=self._in(ars)
         if check == 'YES':
-            data=self.data_of_queue.get_data()
-            data[ars[1]].pop(self.data_of_queue.poisk_num(ars[1], ars[2]))
+            self.data_of_queue.remove(ars[1],ars[2])
         return check
 
     def _restore(self):
@@ -78,7 +67,7 @@ class Server:
                     key = arguments[1]
                     current_task = Task(int(arguments[2]), arguments[3], arguments[4],key)
                     current_task._status=arguments[5]
-                    current_task._date_of_take = datetime.strptime(arguments[6])
+                    current_task._date_of_take = arguments[6]
                     data_dict[key].append(current_task)
                 else:
                     self.data_of_queue._current_id=int(arguments[0])
@@ -102,22 +91,6 @@ class Server:
         archive_file.close()
         return None
 
-    def _check_timeout(self):
-        now_time=datetime.now()
-        current_data=self.data_of_queue.get_data()
-        data_values=list(current_data.values())
-        for current_list in data_values:
-            for task in current_list:
-                if task._date_of_take is not None:
-                    delta = now_time - task._date_of_take
-                    if delta.seconds>300:
-                        self._remove_from_queue(self,task)
-
-    def _remove_from_queue(self,task):
-        self.data_of_queue._data[task._num_of_queue].remove(task)
-        task._status=False
-        task._date_of_take = None
-        self.data_of_queue._data[task._num_of_queue].append(task)
 
 
 if __name__ == '__main__':
